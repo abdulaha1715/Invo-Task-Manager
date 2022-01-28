7a0256bf3e59a27220fe4b9a4f3afb14ec86280c
Abdulaha Islam <abdulahaislam210917@gmail.com>
2022-01-28 20:15:35 +0600

    Created Model - Client, Task

---
+++ database/migrations/2022_01_28_135616_create_tasks_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->longText('description');
+            $table->integer('price');
+            $table->foreignId('client_id');
+            $table->foreignId('user_id');
+            $table->timestamps();
+
+
+
+            // otherWay of define foreign key
+            // $table->unsignedBigInteger('client_id');
+            // $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
